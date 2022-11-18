696e2fe6d111f72cdd71388f46567f40db54f724
Andrii Yakymenko <ayakymenko@playtika.com>
2022-11-18 18:40:20 +0200

    temp

--- src/main/java/com/stranger/gas/model/User.java
+++ src/main/java/com/stranger/gas/model/User.java
@@ -16,6 +16,7 @@ import org.checkerframework.common.aliasing.qual.Unique;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "usr")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
